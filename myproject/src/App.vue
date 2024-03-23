cc86d96d52399acb1a468e19e296b191fe34383e
DarkestTiger <june140133@gmail.com>
2024-03-23 17:20:46 +0900

    .

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,30 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Signup  from './components/Signup.vue'
+import Login  from './components/Login.vue'
+import Logout  from './components/Logout.vue'
+
+const routes = {
+  '/': Home,
+  '/login': 로그인,
+  '/signup': 회원가입
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <a href="#/">Home</a> |
+        <a href="#/signup">회원가입</a> |
+        <a href="#/login">로그인</a> 
+      </div>
+      <div class="col-6">
+        <component :is="currentView" />
+        <Logout />
+      </div>
+      <div class="col">
+        3 of 3
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+</template>>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
